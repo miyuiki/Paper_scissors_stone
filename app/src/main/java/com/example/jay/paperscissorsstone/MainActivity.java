4c6dfd07491208ddf5f1c2ae87e2fd528da68180
Chang Jay <a121406@gmail.com>
2017-03-19 01:43:02 +0800

    0319 PC

--- app/src/main/java/com/example/jay/paperscissorsstone/MainActivity.java
+++ app/src/main/java/com/example/jay/paperscissorsstone/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.jay.paperscissorsstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
     int[] imgId = {R.drawable.paper,R.drawable.scissors,R.drawable.stone};
+
     private ImageButton pap,sci,sto;
     private ImageView imgPhoto;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,43 @@ public class MainActivity extends AppCompatActivity {
         sci = (ImageButton)findViewById(R.id.imageButton2);
         sto = (ImageButton)findViewById(R.id.imageButton3);
         imgPhoto = (ImageView) findViewById(R.id.imageView);
+
+        pap.setOnClickListener(paperListener);
+        sci.setOnClickListener(scissorsListener);
+        sto.setOnClickListener(stoneListener);
+
+    }
+    private ImageButton.OnClickListener paperListener = new ImageButton.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            shuffleArray(imgId);
+            imgPhoto.setImageResource(imgId[0]);
+        }
+    };
+    private ImageButton.OnClickListener scissorsListener = new ImageButton.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            shuffleArray(imgId);
+            imgPhoto.setImageResource(imgId[0]);
+        }
+    };
+    private ImageButton.OnClickListener stoneListener = new ImageButton.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            shuffleArray(imgId);
+            imgPhoto.setImageResource(imgId[0]);
+        }
+    };
+    public static int[] shuffleArray(int[] array)
+    {
+        Random rnd = new Random();
+        for (int i = 0;i < array.length; i++)
+        {
+            int randomPosition = rnd.nextInt(array.length);
+            int temp = array[i];
+            array[i] = array[randomPosition];
+            array[randomPosition] = temp;
+        }
+        return array;
     }
 }
